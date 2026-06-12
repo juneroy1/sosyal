31573b2998d9c3079589f044fe121204da6628f1
Juneroy Quinimon <eroyjune@gmail.com>
2026-06-12 12:04:05 +0800

    first commit

---
+++ cmd/api/api.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// mux.HandleFunc("GET /v1/health", func(w http.ResponseWriter, r *http.Request) {
+	// 	app.healthCheckHandler(w, r)
+	// })
+
+	// grouping route/end points
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
