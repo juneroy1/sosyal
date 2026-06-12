9f08255985edac91b7d6bfbfab99a72d640d57ed
Juneroy Quinimon <eroyjune@gmail.com>
2026-06-12 14:45:38 +0800

    added env internal and functions

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/juneroy1/sosyal/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 	app := &application{
 		config: cfg,
